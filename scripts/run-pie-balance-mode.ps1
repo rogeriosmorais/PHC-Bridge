ec0a66d8d46bef4c4b8492c237e97f7741f53196
Rogerio Morais <rogeriosmorais@gmail.com>
2026-05-07 14:43:37 -0300

    Deleting old test runners

--- scripts/run-pie-balance-mode.ps1
+++
@@ -1,8 +0,0 @@
-[CmdletBinding()]
-param(
-    [string]$EngineRoot = $(if ($env:UE5_PATH) { $env:UE5_PATH } else { "E:\UE_5.7\Engine" }),
-    [string]$ProjectPath = "$PSScriptRoot\..\PhysAnimUE5\PhysAnimUE5.uproject"
-)
-
-$scriptPath = Join-Path $PSScriptRoot "run-pie-smoke.ps1"
-powershell.exe -File $scriptPath -EngineRoot $EngineRoot -ProjectPath $ProjectPath -TestName "PhysAnim.PIE.BalanceMode"
