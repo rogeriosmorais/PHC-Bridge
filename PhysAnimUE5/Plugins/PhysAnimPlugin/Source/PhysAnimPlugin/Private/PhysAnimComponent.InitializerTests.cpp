a8beabd5ee197e0e85b8caa5feaae39c6d09c179
Rogerio Morais <rogeriosmorais@gmail.com>
2026-07-13 09:58:40 -0300

    Author stable Manny passive damping

--- PhysAnimUE5/Plugins/PhysAnimPlugin/Source/PhysAnimPlugin/Private/PhysAnimComponent.InitializerTests.cpp
+++ PhysAnimUE5/Plugins/PhysAnimPlugin/Source/PhysAnimPlugin/Private/PhysAnimComponent.InitializerTests.cpp
@@ -3,6 +3,7 @@
 #include "PhysAnimComponent.h"
 #include "PhysAnimStage1InitializerComponent.h"
 #include "PhysicsEngine/PhysicsAsset.h"
+#include "PhysicsEngine/SkeletalBodySetup.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Engine/SkeletalMesh.h"
 #include "Misc/AutomationTest.h"
@@ -49,6 +50,46 @@ namespace
 		TestNotNull(TEXT("Manny skeletal mesh should load"), MannyMesh);
 		return MannyMesh != nullptr;
 	}
+
+	IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+		FPhysAnimMannyPassivePlantDampingContractTest,
+		"PhysAnim.Component.MannyPassivePlantDampingContract",
+		EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+	bool FPhysAnimMannyPassivePlantDampingContractTest::RunTest(const FString& Parameters)
+	{
+		const UPhysicsAsset* const PhysicsAsset = LoadObject<UPhysicsAsset>(nullptr, TEXT("/Game/Characters/Mannequins/Rigs/PA_Mannequin.PA_Mannequin"));
+		TestNotNull(TEXT("Manny physics asset should load"), PhysicsAsset);
+		if (!PhysicsAsset)
+		{
+			return false;
+		}
+
+		for (const FName BoneName : PhysAnimBridge::GetRequiredBodyModifierBoneNames())
+		{
+			const int32 BodyIndex = PhysicsAsset->FindBodyIndex(BoneName);
+			TestTrue(*FString::Printf(TEXT("Required body exists: %s"), *BoneName.ToString()), PhysicsAsset->SkeletalBodySetups.IsValidIndex(BodyIndex));
+			if (!PhysicsAsset->SkeletalBodySetups.IsValidIndex(BodyIndex) || !PhysicsAsset->SkeletalBodySetups[BodyIndex])
+			{
+				continue;
+			}
+
+			const FBodyInstance& Body = PhysicsAsset->SkeletalBodySetups[BodyIndex]->DefaultInstance;
+			AddInfo(FString::Printf(
+				TEXT("PASSIVE_PLANT_DAMPING body=%s linear=%.3f angular=%.3f"),
+				*BoneName.ToString(),
+				Body.LinearDamping,
+				Body.AngularDamping));
+			TestTrue(
+				*FString::Printf(TEXT("%s linear damping is authored at 0.1"), *BoneName.ToString()),
+				FMath::IsNearlyEqual(Body.LinearDamping, 0.1f));
+			TestTrue(
+				*FString::Printf(TEXT("%s angular damping is authored at 1.0"), *BoneName.ToString()),
+				FMath::IsNearlyEqual(Body.AngularDamping, 1.0f));
+		}
+		return true;
+	}
+
 }
 
 #endif
