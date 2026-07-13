a0134ed55b6c9a870fc0ffcd83c497296ba9cab6
Rogerio Morais <rogeriosmorais@gmail.com>
2026-07-13 13:28:44 -0300

    Bound standing control extra damping

--- PhysAnimUE5/Plugins/PhysAnimPlugin/Source/PhysAnimPlugin/Private/PhysAnimComponent.InitializerTests.cpp
+++ PhysAnimUE5/Plugins/PhysAnimPlugin/Source/PhysAnimPlugin/Private/PhysAnimComponent.InitializerTests.cpp
@@ -33,7 +33,7 @@ namespace
 			TestFalse(*FString::Printf(TEXT("%s remains disabled until atomic standing publication"), *Pair.Key.ToString()), Pair.Value.ControlData.bEnabled);
 			TestEqual(*FString::Printf(TEXT("%s uses an 8 Hz angular spring"), *Pair.Key.ToString()), Pair.Value.ControlData.AngularStrength, 8.0f);
 			TestEqual(*FString::Printf(TEXT("%s starts critically damped"), *Pair.Key.ToString()), Pair.Value.ControlData.AngularDampingRatio, 1.0f);
-			TestEqual(*FString::Printf(TEXT("%s uses plant-scale extra damping"), *Pair.Key.ToString()), Pair.Value.ControlData.AngularExtraDamping, 50.0f);
+			TestEqual(*FString::Printf(TEXT("%s uses solver-stable extra damping"), *Pair.Key.ToString()), Pair.Value.ControlData.AngularExtraDamping, 10.0f);
 		}
 		FInitialPhysicsControl* const MutatedControl = Initializer->InitialControls.Find(PhysAnimBridge::MakeControlName(TEXT("thigh_l")));
 		TestNotNull(TEXT("Thigh control exists for runtime contract check"), MutatedControl);
@@ -43,7 +43,7 @@ namespace
 			MutatedControl->ControlData.AngularExtraDamping = 1.0f;
 			Initializer->PrepareRuntimeDefaults();
 			TestEqual(TEXT("Runtime defaults replace stale serialized strength"), MutatedControl->ControlData.AngularStrength, 8.0f);
-			TestEqual(TEXT("Runtime defaults replace stale serialized extra damping"), MutatedControl->ControlData.AngularExtraDamping, 50.0f);
+			TestEqual(TEXT("Runtime defaults replace stale serialized extra damping"), MutatedControl->ControlData.AngularExtraDamping, 10.0f);
 		}
 		return true;
 	}
